d2e3359110d29c1afcfd415ed1bcc2d20961a60b
belljohan3 <belljohan3@gmail.com>
2022-08-29 17:14:12 +0100

    To-Do list with Pinia Library

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form @submit.prevent="addItemAndClear(todo)">
+    <input v-model="todo" type="text" /> <button>+ Add a Task</button>
+  </form>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+  import { useTodoListStore } from "../stores/todoList";
+
+  const todo = ref("");
+  const store = useTodoListStore();
+
+  function addItemAndClear(item) {
+    if (item.length === 0) {
+      return
+    }
+    // invokes function in the store:
+    store.addTodo(item)
+    todo.value = ''
+  }
+</script>
+
+<style scoped>
+  form {
+    margin: 15px;
+  }
+  input {
+    margin-top: 25px;
+    margin-bottom: 15px;
+    height: 20px;
+    width: 50%;
+  }
+  button {
+    margin-left: 30px;
+    background-color: #2f6089;
+    padding: 5px 10px;
+    border: none;
+    border-radius: 5px;
+    font-weight: 800;
+    color: white;
+    width: 30%;
+    height: 30%;
+  }
+  .alert-div {
+    min-height: 25px;
+  }
+  .alert {
+    color: #d1495b;
+    font-size: 1em;
+    font-weight: 600;
+  }
+  </style>
